264b0491a69cf12ddac27b02396eb40e7dfcab91
AlexLaim <92443002+AlexLaim@users.noreply.github.com>
2022-03-06 16:22:10 +0300

    .

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -139,7 +139,6 @@ namespace lab5
             bool flag = true;
             while (flag)
             {
-                bool flag2 = true;
                 Console.WriteLine("Меню:");
                 Console.WriteLine("1 – Просмотр таблицы\n2 – Добавить запись\n3 – Удалить запись\n4 – Обновить запись\n5 – Поиск записей\n6 – Просмотреть лог\n7 - Выход");
                 Console.WriteLine("Выберите действие:");
@@ -160,36 +159,40 @@ namespace lab5
                         Console.WriteLine(new String('-', 76));
                         break;
                     case "2":
-                        
-                            Console.WriteLine("Введите данные:");
-                            Console.WriteLine("ФИО:");
-                            string Name = Console.ReadLine();
+                        string Name = string.Empty;
+                        int Age, Exp;
+                        types Types;
+                        Console.WriteLine("Введите данные:");
+                        for (; ; ) {                         
+                        Console.WriteLine("ФИО:");
+                         Name = Console.ReadLine();
 
-                            //---> ПРОВЕРКА НА НАЛИЧИЕ ЦИФР В СТРОКЕ
-                            //foreach (var symbol in Name)
-                            //    if (!char.IsDigit(symbol))
-                            //    {
-                            //        flag2 = false;
+                        //---> ПРОВЕРКА НА НАЛИЧИЕ ЦИФР В СТРОКЕ
+                        //foreach (var symbol in Name)
+                        //    if (!char.IsDigit(symbol))
+                        //    {
+                        //        flag2 = false;
 
 
-                            //    }
-                            //    else
-                            //    {
-                            //        Console.WriteLine("Не корректные данные, введите ФИО еще раз.");
-                            //        break;
-                            //    }
+                        //    }
+                        //    else
+                        //    {
+                        //        Console.WriteLine("Не корректные данные, введите ФИО еще раз.");
+                        //        break;
+                        //    }
 
-                            //---> ПРОВЕРКА СТРОКИ, КОТОРАЯ СОСТОИТ ТОЛЬКО ИЗ БУКВ
-                            //if (Regex.IsMatch(Name, @"^[a-zA-Z]+$"))
-                            //{
-                            //    flag2 = false;
-                            //}
-                            //else
-                            //{
-                            //    Console.WriteLine("Некорректные данные, введите ФИО еще раз.");
-                            //}
+                       // --->ПРОВЕРКА СТРОКИ, КОТОРАЯ СОСТОИТ ТОЛЬКО ИЗ БУКВ
+                            if (Regex.IsMatch(Name, @"^[a-zA-Z]+$"))
+                        {
                             
-                        
+                                break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Некорректные данные, введите ФИО еще раз.");
+                        }
+                }
+
                         //---> ПРОВЕРКА СТРОКИ, КОТОРАЯ СОСТОИТ ТОЛЬКО ИЗ ЦИФР
                         //if (Regex.IsMatch(Name, @"^[0-9]+$"))
                         //{
@@ -199,15 +202,69 @@ namespace lab5
                         //{
                         //    Console.WriteLine("Некорректные данные, введите ФИО еще раз.");
                         //}
+                        for (; ; )
+                        {
+                            try
+                            {
+                                Console.WriteLine("Тип деятельности: Т-тренер - 0, С-спортсмен - 1");
+                                int del2 = int.Parse(Console.ReadLine());
+                                Types = (types)del2;                               
+                                if (del2 == 0 || del2 == 1)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректные данные, введите тип деятельности еще раз.");
+                                }
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение!");
+                            }
+                        }
 
-                        Console.WriteLine("Тип деятельности: Т-тренер - 0, С-спортсмен - 1");
-                        types Types = (types)int.Parse(Console.ReadLine());
+                        for (; ; )
+                        {
+                            try
+                            {
+                                Console.WriteLine("Введите год рождения:");
+                                Age = int.Parse(Console.ReadLine());
+                                if (Age >= 0)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректные данные, введите год рождения еще раз.");
+                                }
+                            }
+                            catch (FormatException) 
+                            { 
+                                Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение!"); 
+                            }
+                        }
 
-                        Console.WriteLine("Введите год рождения:");
-                        int Age = int.Parse(Console.ReadLine());
-
-                        Console.WriteLine("Введите опыт работы(лет):");
-                        int Exp = int.Parse(Console.ReadLine());
+                        for (; ; )
+                        {                           
+                            try
+                            {
+                                Console.WriteLine("Введите опыт работы(лет):");
+                                Exp = int.Parse(Console.ReadLine());
+                                if (Exp >= 0)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректные данные, введите опыт работы еще раз.");
+                                }
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение!");
+                            }
+                        }
                         Item value = new(Name, Types, Age, Exp);
                         list.Add(value);
                         DateTime time = DateTime.Now;
@@ -250,33 +307,139 @@ namespace lab5
                         act = actions.upd;
                         val = new(act, name, time);
                         list3.Add(val);
+                        string Name2 = string.Empty;
+                        types Types2;
+                        int Age2, Exp2;
                         Console.WriteLine("Введите данные:");
 
-                        Console.WriteLine("ФИО:");
-                        string Name2 = Console.ReadLine();
+                        for (; ; )
+                        {
+                            Console.WriteLine("ФИО:");
+                            Name2 = Console.ReadLine();
 
-                        Console.WriteLine("Тип деятельности: Т-тренер - 0, С-спортсмен - 1");
-                        types Types2 = (types)int.Parse(Console.ReadLine());
+                            if (Regex.IsMatch(Name2, @"^[a-zA-Z]+$"))
+                            {
 
-                        Console.WriteLine("Введите год рождения:");
-                        int Age2 = int.Parse(Console.ReadLine());
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Некорректные данные, введите ФИО еще раз.");
+                            }
+                        }
 
-                        Console.WriteLine("Введите опыт работы(лет):");
-                        int Exp2 = int.Parse(Console.ReadLine());
+                        for (; ; )
+                        {
+                            try
+                            {
+                                Console.WriteLine("Тип деятельности: Т-тренер - 0, С-спортсмен - 1");
+                                int del2 = int.Parse(Console.ReadLine());
+                                Types2 = (types)del2;
+                                if (del2 == 0 || del2 == 1)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректные данные, введите тип деятельности еще раз.");
+                                }
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение!");
+                            }
+                        }
+
+                        for (; ; )
+                        {
+                            try
+                            {
+                                Console.WriteLine("Введите год рождения:");
+                                Age2 = int.Parse(Console.ReadLine());
+                                if (Age2 >= 0)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректные данные, введите год рождения еще раз.");
+                                }
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение!");
+                            }
+                        }
+
+                        for (; ; )
+                        {
+                            try
+                            {
+                                Console.WriteLine("Введите опыт работы(лет):");
+                                Exp2 = int.Parse(Console.ReadLine());
+                                if (Exp2 >= 0)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректные данные, введите опыт работы еще раз.");
+                                }
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение!");
+                            }
+                        }
                         list[num2 - 1] = new Item(Name2, Types2, Age2, Exp2);
                 
                 break;
             
                     case "5":
                         Console.WriteLine("Введите тип поиска: ");
-                        Console.WriteLine("1 - поиск по ФИО, \n2 - поиск по году рождения \n3 - по типу деятельности (Т-тренер, С-спортсмен)");
-                        string var = string.Empty;
-                        List<Item> list2 = new List<Item>();
-                        switch (Console.ReadLine().Trim())
+                        Console.WriteLine("1 - поиск по ФИО \n2 - поиск по году рождения \n3 - по типу деятельности (Т-тренер, С-спортсмен)");
+                        int del;
+                        for (; ; )
                         {
-                            case "1":
-                                Console.WriteLine("Введите ФИО: ");
-                                var = Console.ReadLine();
+                            try
+                            {                                
+                                 del = int.Parse(Console.ReadLine());                                
+                                    if (del == 1 || del == 2 || del == 3)
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение от 1 до 3!");
+                                    }                                
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение!");
+                            }
+                        }
+                        string var = string.Empty;
+                        int var2;
+                        types var3;
+                        List<Item> list2 = new List<Item>();
+                        switch (del)
+                        {
+                            case 1:
+                                for (; ; )
+                                {
+                                    Console.WriteLine("Введите ФИО:");
+                                    var = Console.ReadLine();
+
+                                    if (Regex.IsMatch(var, @"^[a-zA-Z]+$"))
+                                    {
+
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Некорректные данные, введите ФИО еще раз.");
+                                    }
+                                }
                                 foreach (Item item in list)
                                 {
                                     if (item.Name.Equals(var))
@@ -295,9 +458,27 @@ namespace lab5
                                 Console.WriteLine($"{"|Перечисляемый тип: Т - тренер, С - спортсмен",-75}|");
                                 Console.WriteLine(new String('-', 76));
                                 break;
-                            case "2":
-                                Console.WriteLine("Введите год рождения");
-                                int var2 = int.Parse(Console.ReadLine());
+                            case 2:                               
+                                for (; ; )
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("Введите год рождения:");
+                                        var2 = int.Parse(Console.ReadLine());
+                                        if (var2 >= 0)
+                                        {
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Некорректные данные, введите год рождения еще раз.");
+                                        }
+                                    }
+                                    catch (FormatException)
+                                    {
+                                        Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение!");
+                                    }
+                                }
                                 foreach (Item item in list)
                                 {
                                     if (item.Age.Equals(var2))
@@ -316,9 +497,28 @@ namespace lab5
                                 Console.WriteLine($"{"|Перечисляемый тип: Т - тренер, С - спортсмен",-75}|");
                                 Console.WriteLine(new String('-', 76));
                                 break;
-                            case "3":
-                                Console.WriteLine("Введите тип деятельности: Т- тренер - 0, С - спортсмен - 1");
-                                types var3 = (types)int.Parse(Console.ReadLine());
+                            case 3:
+                                for (; ; )
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("Тип деятельности: Т-тренер - 0, С-спортсмен - 1");
+                                        int del2 = int.Parse(Console.ReadLine());
+                                        var3 = (types)del2;
+                                        if (del2 == 0 || del2 == 1)
+                                        {
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Некорректные данные, введите тип деятельности еще раз.");
+                                        }
+                                    }
+                                    catch (FormatException)
+                                    {
+                                        Console.WriteLine("Некорректные данные, пожалуйста введите числовое значение!");
+                                    }
+                                }
                                 foreach (Item item in list)
                                 {
                                     if (item.Type.Equals(var3))
@@ -346,6 +546,10 @@ namespace lab5
                             {
                                 logs.Print();
                             }
+                            if (list3.Count == 0)
+                            {
+                                Console.WriteLine("Лог пуст!");
+                            }
                             TimeSpan varr = list3[1].Time - list3[0].Time;
                             for (int i = 2; i < list3.Count; i++)
                             {
@@ -355,13 +559,15 @@ namespace lab5
                                     varr = varr2;
                                 }
                             }
+                            
                             Console.WriteLine();
                             Console.WriteLine($"{varr} - Самый долгий период бездействия пользователя");
                         }
-                        catch (Exception)
+                        catch (ArgumentOutOfRangeException)
                         {
-                            Console.WriteLine("Лог пуст!");
+                            
                         }
+                        
                         break;
                     case "7":
 
