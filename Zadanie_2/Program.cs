b878e5e8dae09924b2e3e80baceaa1a2c0075684
AlexLaim <92443002+AlexLaim@users.noreply.github.com>
2022-03-07 17:27:47 +0300

    Lab6

---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+       
+    
+        static void Main(string[] args)
+        {
+            //ВАРИАНТ 7
+            Console.WriteLine("Программа начала работу");
+            
+            
+            string filename = Directory.GetCurrentDirectory() + "\\file1.dat";
+            using (BinaryWriter bw = new BinaryWriter(File.Open(filename, FileMode.OpenOrCreate)))
+            {
+                for (int M = 1; M <= 5; M++)
+                {
+                    for (int N = 1; N <= 5; N++)
+                    {
+                        bw.Write(M);
+                        bw.Write(N);
+                        bw.Write(Math.Pow(M, N));
+                        
+                    }
+                }               
+            }
+            foo();
+        }
+
+        static void foo()
+        {   
+            string filename1 = Directory.GetCurrentDirectory() + "\\file1.dat";
+            string filename2 = Directory.GetCurrentDirectory() + "\\file2.dat";
+            using (BinaryReader br = new BinaryReader(File.Open(filename1, FileMode.Open)))
+            {
+                using (BinaryWriter bw = new BinaryWriter(File.Open(filename2, FileMode.OpenOrCreate)))
+                {                                           
+                        br.BaseStream.Position += 3;
+                        bw.Write(br.ReadDouble());                                                               
+                }
+            }
+            Console.WriteLine("Программа отработала без ошибок");
+        }
+    }
+}
