5cf50c1b3e0fcbcc13483c2b0f05a709d09f51fa
AlexLaim <92443002+AlexLaim@users.noreply.github.com>
2022-04-16 21:19:28 +0300

    .

--- Zadanie_3/Program.cs
+++ Zadanie_3/Program.cs
@@ -6,8 +6,50 @@ namespace Zadanie_3
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {          
+            string filename3 = Directory.GetCurrentDirectory() + "\\file1.txt";
+            string newfilename3 = Directory.GetCurrentDirectory() + "\\file2.txt";           
+            int k = 0;
+            File.Create(newfilename3).Close();
+            Console.WriteLine("Текст файла: ");
+            using (StreamReader sr = new StreamReader(filename3))
+            {
+                using (StreamWriter streamwriter = new StreamWriter(newfilename3, false))
+                {                  
+                    while (!sr.EndOfStream)
+                    {                        
+                        string[] Array2 = sr.ReadLine().Split();
+                        for (int i = 0; i < Array2.Length; i++)
+                        {
+
+                            string str = Array2[i];
+                            Console.WriteLine(str);
+                            if (str == string.Empty)
+                            {
+                                streamwriter.WriteLine(str);
+                                k++;
+                            }
+                            else if (str != string.Empty)
+                            {
+                                streamwriter.WriteLine(str + "(c)Student");
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Количество пустых строк: " + k);
+            using (StreamReader sr = new StreamReader(newfilename3))
+            {
+                while (!sr.EndOfStream)
+                {                   
+                    string[] Array2 = sr.ReadLine().Split();
+                    for (int i = 0; i < Array2.Length; i++)
+                    {
+                        string str = Array2[i];
+                        Console.WriteLine(str);
+                    }
+                }
+            }
         }
     }
 }
